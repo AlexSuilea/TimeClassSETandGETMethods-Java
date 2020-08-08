8553523f6ebc5630adb5dc7085707129ee30070b
Alexandru Suilea <42109259+AlexSuilea@users.noreply.github.com>
2020-08-08 10:53:36 +0300

    Add files via upload

---
+++ TimeClassSETandGETMethods/src/com/alexsuilea/Main.java
@@ -0,0 +1,9 @@
+package com.alexsuilea;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Time time = new Time(12,43,28);
+        System.out.println(time.toMilitary());
+    }
+}
